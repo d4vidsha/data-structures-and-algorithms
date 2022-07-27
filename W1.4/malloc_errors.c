80576573992eea741ab6c919372c73ad647b905c
d4vidsha <david.sha2002@gmail.com>
2022-07-27 10:28:00 +1000

    malloc errors

--- W1.4/malloc_errors.c
+++ W1.4/malloc_errors.c
@@ -12,7 +12,7 @@
 // malloc will return NULL if it fails, asserts are a quick way to check success.
 #include <assert.h>
 
-#define NUMBEROFINTS 1
+#define NUMBEROFINTS 2
 
 int main(int argc, char **argv){
     // We declare space to store a pointer of type int.
@@ -22,12 +22,15 @@ int main(int argc, char **argv){
     // Here we allocate space for two integers. The result is a generic pointer,
     // so we also set the type to a pointer to integers.
     values = (int *) malloc(sizeof(int) * NUMBEROFINTS);
+    assert(values);
 
     // Set each of the integers.
     values[0] = 10;
+    values[1] = 1;
 
     // Print the values out.
     printf("%d\n", values[0]);
+    printf("%d\n", values[1]);
 
     // free the malloced memory.
     free(values);
