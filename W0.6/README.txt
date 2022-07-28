2447a7d9634304ea1d572c6b1f1fdccf3e807235
d4vidsha <david.sha2002@gmail.com>
2022-07-28 10:42:16 +1000

    initialise

---
+++ W0.6/README.txt
@@ -0,0 +1,3 @@
+All the data files in this folder are extracted from:
+https://en.wikipedia.org/wiki/List_of_cities_in_Australia_by_population
+on 12/DEC/2021
---
+++ W0.6/program.c
@@ -0,0 +1,15 @@
+/* Skeleton for W0.6/E0.15, comp20003 workshop Week 0 */
+
+// A Touch Of Reality
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char **argv){
+
+
+
+    return EXIT_SUCCESS;
+}
