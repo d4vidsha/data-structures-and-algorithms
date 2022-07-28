05d025045a1d43c34d9fb3eb7ebbdeb693d37e16
d4vidsha <david.sha2002@gmail.com>
2022-07-28 13:04:55 +1000

    organise alphabetically and highest population

--- W0.6/program.c
+++ W0.6/program.c
@@ -28,6 +28,7 @@ int build_city_array(city_t A[]);
 void print_city(city_t *ct);
 void print_city_array(city_t A[], int n);
 void organise_alphabetically(city_t A[], int n);
+void highest_population(city_t A[], int n);
 
 int main(int argc, char **argv){
 
@@ -47,11 +48,14 @@ int main(int argc, char **argv){
     int n = build_city_array(cities);
 
     // organise into alphabetical order
+    organise_alphabetically(cities, n);
 
     // print to stdout list of cities in alphabetical order
     printf("LIST OF CITIES IN ALPHABETICAL ORDER\nCity, Population June 2020, %% of national population June 2019\n");
     print_city_array(cities, n);
 
+    // highest population city
+    highest_population(cities, n);
 
     return EXIT_SUCCESS;
 }
@@ -118,5 +122,38 @@ void print_city_array(city_t A[], int n) {
 /* Organise cities in alphabetical order.
 */
 void organise_alphabetically(city_t A[], int n) {
-    
+    for (int i = 0; i < n; i++) {
+        city_t *ct1 = &A[i];
+        char *name1 = ct1->name;
+        for (int j = i + 1; j < n; j++) {
+            city_t *ct2 = &A[j];
+            char *name2 = ct2->name;
+            // printf("A[%d]->name: %s\n", i, name1);
+            // printf("A[%d]->name: %s\n", i, name2);
+            if (strcmp(name1, name2) > 0) {
+                city_t temp = *ct1;
+                ct1 = ct2;
+                ct2 = &temp;
+                A[i] = *ct1;
+                A[j] = *ct2;
+                // printf("A[%d]->name: %s\n", i, ct1->name);
+                // printf("A[%d]->name: %s\n", i, ct2->name);
+            }
+            // printf("\n");
+        }
+    }
+}
+
+/*  Given an array of cities, find the city with the highest population and
+    print to stdout.
+*/
+void highest_population(city_t A[], int n) {
+    city_t *max = A + 0;    // A + 0 == A == &A[0]
+
+    for (int i = 0; i < n; i++) {
+        if (A[i].population > max->population) {
+            max = A + i;
+        }
+    }
+    return max;
 }
