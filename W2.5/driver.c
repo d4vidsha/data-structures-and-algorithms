ad007f90a005661a5735b1b38767b83e4436f8f3
d4vidsha <david.sha2002@gmail.com>
2022-08-04 12:02:37 +1000

    updated

--- W2.5/driver.c
+++ W2.5/driver.c
@@ -12,23 +12,41 @@
 #include "data.h"
 #include "array.h"
 
+#define NUM_ARGS 3
+
 int main(int argc, char *argv[]) {
 	// FILL IN: check to have at least 3 arguments in command line
 	//          open input and output files
+	if (argc < NUM_ARGS) {
+		printf("Error: Need at least %d arguments.\n", NUM_ARGS);
+		return EXIT_FAILURE;
+	}
+
+	printf("%s\n%s\n", argv[1], argv[2]);
+
+	char *inFilename = (char *)malloc((strlen(argv[1]) + 1) * sizeof(char));
+	char *outFilename = (char *)malloc((strlen(argv[2]) + 1) * sizeof(char));
+	strcpy(inFilename, argv[1]);
+	strcpy(outFilename, argv[2]);
+	FILE *inFile = fopen(inFilename, "r");
+	FILE *outFile = fopen(outFilename, "w");
+	free(inFilename);
+	free(outFilename);
+
 	student_t *students[MAX_STUDENTS];
 	           // students now is an array of pointers to struct 
 	int n = 0;   // number of students 
 	
 	// FILL IN: some of the lines below need to be changed
-	n = buildStudentArray(students);
+	n = buildStudentArray(inFile, students);
 
-	printStudentArray(students, n);
+	printStudentArray(outFile, students, n);
 
-	printf("A student with highest score:\n");
-	printStudent(bestStudent(students, n));
+	fprintf(outFile, "A student with highest score:\n");
+	printStudent(outFile, bestStudent(students, n));
 
 	// prints out the memory in KB
-	printf("\nMemory used: %ld KB\n\n", bytesUsedByArray(students, n) >> 10);
+	fprintf(outFile, "\nMemory used: %d KB\n\n", bytesUsedByArray(students, n) >> 10);
 
 	// free all the momory that were malloc-ed:
 	freeStudentArray(students, n);
