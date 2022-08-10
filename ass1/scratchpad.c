d174931b3e6223680f4e60eb908a8b823a42795e
d4vidsha <david.sha2002@gmail.com>
2022-08-10 14:47:28 +1000

    added scratch

--- ass1/scratchpad.c
+++ ass1/scratchpad.c
@@ -2,6 +2,51 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    // int footpath_id;
+    // char address[MAX_STR_LEN + NULLBYTE];
+    // char clue_sa[MAX_STR_LEN + NULLBYTE];
+    // char asset_type[MAX_STR_LEN + NULLBYTE];
+    // double deltaz;
+    // double distance;
+    // double grade1in;
+    // int mcc_id;
+    // int mccid_int;
+    // double rlmax;
+    // double rlmin;
+    // char segside[MAX_STR_LEN + NULLBYTE];
+    // int statusid;
+    // int streetid;
+    // int street_group;
+    // double start_lat;
+    // double start_lon;
+    // double end_lat;
+    // double end_lon;
+
+    // if (read_fields == NUM_FIELDS) {
+        // fp->footpath_id = footpath_id;
+        // strcpy(fp->address, address);
+        // strcpy(fp->clue_sa, clue_sa);
+        // strcpy(fp->asset_type, asset_type);
+        // fp->deltaz = deltaz;
+        // fp->distance = distance;
+        // fp->grade1in = grade1in;
+        // fp->mcc_id = mcc_id;
+        // fp->mccid_int = mccid_int;
+        // fp->rlmax = rlmax;
+        // fp->rlmin = rlmin;
+        // strcpy(fp->segside, segside);
+        // fp->statusid = statusid;
+        // fp->streetid = streetid;
+        // fp->street_group = street_group;
+        // fp->start_lat = start_lat;
+        // fp->start_lon = start_lon;
+        // fp->end_lat = end_lat;
+        // fp->end_lon = end_lon;
+    // } else {
+    //     fprintf(stderr, "ERROR: failed to scan all required fields\n");
+    // }
     printf("%d %d\n", EXIT_SUCCESS, EXIT_FAILURE);
     return EXIT_SUCCESS;
 }
+
