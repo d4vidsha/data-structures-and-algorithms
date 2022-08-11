07cceab5e22d60d17c861d39442afa63e24cd00a
d4vidsha <david.sha2002@gmail.com>
2022-08-11 17:40:31 +1000

    format

--- ass1/stage.h
+++ ass1/stage.h
@@ -7,12 +7,7 @@
 #define _STAGE_H_
 
 /* function prototypes ====================================================== */
-/*  Stage 1 of project.
-*/
 void stage1(FILE *in, FILE *out);
-
-/*  Stage 2 of project.
-*/
 void stage2(FILE *in, FILE *out);
 
 #endif
