17c1f9b9f6a8fad65873cc618b9b5359f0892293
d4vidsha <david.sha2002@gmail.com>
2022-08-14 21:00:31 +1000

    failing to find out solution

--- ass1/quicksort.c
+++ ass1/quicksort.c
@@ -49,46 +49,56 @@ node_t *partition(node_t *low, node_t *high, int col) {
     node_t *pivot = high;
     node_t *i = low;
     node_t *j = low;
-
+    node_t *i_next, *j_next;
     while (j != NULL && j != high) {
         double cmp = cmp_column(col, j->fp, pivot->fp);
 
         if (cmp < 0) {
             // j is less than pivot
+            assert(i);
+            assert(j);
+            i_next = i->next;
+            j_next = j->next;
             swap(i, j);
-            i = i->next;
+            i = i_next;
+            j = j_next;
+        } else {
+            j = j->next;
         }
-
-        j = j->next;
     }
+    assert(i);
+    assert(pivot);
     swap(i, pivot);
 
     return pivot;
 }
 
-/*  Swaps the values inside the nodes. Note that it does NOT swap the nodes
-    themselves.
+/*  Swaps the nodes by swapping `next` and `prev`.
 */
 void swap(node_t *n1, node_t *n2) {
-    
-    
+    assert(n1 && n2);
     node_t *next, *prev;
     next = n1->next;
     prev = n1->prev;
-    // assert(n1->next != NULL);
-    // assert(n1->prev != NULL);
-    // assert(n2->next != NULL);
-    // assert(n2->prev != NULL);
     n1->next = n2->next;
     n1->prev = n2->prev;
     n2->next = next;
     n2->prev = prev;
 
-    // node_t *temp;
-    // temp = n1;
-    // n1 = n2;
-    // n2 = temp;
-
+    // assert(n1->next != NULL);
+    // assert(n1->prev != NULL);
+    // assert(n2->next != NULL);
+    // assert(n2->prev != NULL);
+    // if (n1->next == NULL) {
+    //     next = NULL;
+    // } else {
+    //     next = n1->next;
+    // }
+    // if (n1->prev == NULL) {
+    //     prev = NULL;
+    // } else {
+    //     prev = n1->prev;
+    // }
     // if (n2->next == NULL) {
     //     n1->next = NULL;
     // } else {
