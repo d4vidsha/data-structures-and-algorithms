89581460331dc2098549f9cc1ba953eb197d0105
d4vidsha <david.sha2002@gmail.com>
2022-08-30 08:38:11 +1000

    defining quadtree structs

--- ass2/quadtree.h
+++ ass2/quadtree.h
@@ -6,6 +6,41 @@
 #ifndef _QUADTREE_H_
 #define _QUADTREE_H_
 
+/* #includes ================================================================ */
+
+
+/* structures =============================================================== */
+// point in 2D
+typedef struct point2D point2D_t;
+struct point2D {
+    long double x;
+    long double y;
+};
+
+// rectangle region is specified by bottom-left `bl` and top-right `tr` points
+typedef struct rectangle2D rectangle2D_t;
+struct rectangle2D {
+    point2D_t *bl;
+    point2D_t *tr;
+};
+
+// data point containing footpath segment and location of footpath segment
+typedef struct datapoint datapoint_t;
+struct datapoint {
+    footpath_segment_t *fp;
+    point2D_t *loc;
+};
+
+// quadtree node
+typedef struct qtnode qtnode_t;
+struct qtnode {
+    rectangle2D_t *region;
+    qtnode_t *nw;
+    qtnode_t *ne;
+    qtnode_t *sw;
+    qtnode_t *se;
+};
+
 /* function prototypes ====================================================== */
 
 
