b5384930c495f0672b7bb4000ae055f4c33cc401
David Sha <70577814+d4vidsha@users.noreply.github.com>
2022-09-05 12:55:03 +1000

    .

--- ass2/driver.h
+++ ass2/driver.h
@@ -6,9 +6,13 @@
 #ifndef _DRIVER_H_
 #define _DRIVER_H_
 
+/* #includes ================================================================ */
+#include "quadtree.h"
+
 /* function prototypes ====================================================== */
 int num_args_match(int num_args, int argc);
 void exit_failure_with_man();
+rectangle2D_t *parse_rectangle(char *argv[]);
 
 #endif
 /* =============================================================================
