8567034a209c85beae53e18e80680dae92a95bf5
David Sha <70577814+d4vidsha@users.noreply.github.com>
2022-09-07 21:20:45 +1000

    moved search quadtree functions to search.c

--- ass2/search.h
+++ ass2/search.h
@@ -1,5 +1,5 @@
 /* =============================================================================
-   Project: Assignment 2 (imported from Assignment 1)
+   Project: Assignment 2 (altered from Assignment 1)
    search.h :
             = all related to searching
 ============================================================================= */
@@ -9,11 +9,16 @@
 /* #includes ================================================================ */
 #include "data.h"
 #include "linkedlist.h"
+#include "quadtree.h"
+#include "print.h"
 
 /* function prototypes ====================================================== */
 list_t *find_addresses(char *address, list_t *list);
 list_t *linearsearch(double value, list_t *list);
 array_t *binarysearch(double val, array_t *A);
+dpll_t *search_quadtree(qtnode_t *root, point2D_t *p);
+dpll_t *range_search_quadtree(qtnode_t *root, rectangle2D_t *range);
+void _range_search_quadtree(dpll_t *res, qtnode_t *root, rectangle2D_t *range);
 
 #endif
 /* =============================================================================
