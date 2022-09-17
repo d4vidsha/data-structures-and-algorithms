f492180dff8181c9992cb7b6083d7440cf9493ce
David Sha <70577814+d4vidsha@users.noreply.github.com>
2022-09-17 11:30:57 +1000

    save

--- W8.4/mergeInt.c
+++ W8.4/mergeInt.c
@@ -16,7 +16,7 @@ int *mergeSort(int *list, int size){
     }
 
     while (q->num > 1) {
-        qnode_t *qn1, qn2, qn3;
+        qnode_t *qn1, *qn2, *qn3;
         qn1 = dequeue(q);
         qn2 = dequeue(q);
         qn3 = merge(qn1, qn2);
@@ -36,7 +36,41 @@ int *mergeSort(int *list, int size){
 qnode_t *merge(qnode_t *qn1, qnode_t *qn2) {
     qnode_t *merged;
 
-    
+    // malloc space for merged
+    merged = (qnode_t *)malloc(sizeof(*merged));
+    assert(merged);
+    merged->items = (int *)malloc(sizeof(*merged->items) * (qn1->num + qn2->num));
+    assert(merged->items);
+    merged->num = qn1->num + qn2->num;
 
+    int i = 0, j = 0, k = 0;
+    // merge two lists
+    while (i < qn1->num && j < qn2->num) {
+        if (qn1->items[i] < qn2->items[j]) {
+            merged->items[k] = qn1->items[i];
+            i++;
+        } else {
+            merged->items[k] = qn2->items[j];
+            j++;
+        }
+        k++;
+    }
+
+    // copy remaining elements
+    while (i < qn1->num) {
+        merged->items[k] = qn1->items[i];
+        i++;
+        k++;
+    }
+    while (j < qn2->num) {
+        merged->items[k] = qn2->items[j];
+        j++;
+        k++;
+    }
+
+    free(qn1->items);
+    free(qn1);
+    free(qn2->items);
+    free(qn2);
     return merged;
 }
