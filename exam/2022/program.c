2cf8d01b245602345d598270cdfe28e6ca405c9d
David Sha <70577814+d4vidsha@users.noreply.github.com>
2022-11-06 21:33:15 +1100

    boilerplating for exam

--- exam/2022/program.c
+++ exam/2022/program.c
@@ -1,42 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int total_dist(int x, int nRel, int* stNum) {
-    int total = 0;
-    for (int i = 0; i < nRel; i++) {
-        total += abs(x - stNum[i]);
-    }
-    return total;
-}
+
 
 int main(int argc, char* argv[]) {
 
-    int nRel;
-    scanf("%d", &nRel);
-
-    // initialise array to store street numbers
-    int* stNum = (int *)malloc(sizeof(int) * nRel);
-    assert(stNum);
-
-    // populate array with street numbers
-    for (int i = 0; i < nRel; i++) {
-        stNum[i] = rand() % 100;
-    }
-    
-    // find the minimum distance for each street number
-    int min = INFINITY;
-    int dist;
-    int minStNum;
-    for (int i = 0; i < nRel; i++) {
-        dist = total_dist(stNum[i], nRel, stNum);
-        if (dist < min) {
-            minStNum = stNum[i];
-            min = dist;
-        }
-    }
-
-    printf("Street number: %d\nTotal distance: %d\n", minStNum, min);
-
-    free(stNum);
 }
+
